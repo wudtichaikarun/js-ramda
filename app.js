24153315cc22b30ae781a7589c20d48c583846ad
romantic <cesc_fabregas1414@hotmail.com>
2018-10-28 21:03:35 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var objects = [
+  {
+    id: 11,
+    name: 'romantic1'
+  },
+  {
+    id: 22,
+    name: 'romantic2'
+  },
+  {
+    id: 33,
+    name: 'romantic3'
+  }
+];
+
+const obj1 = {
+  id: 111,
+  name: 'romantic'
+};
+
+// const ids = objects.map(obj => obj.id);
+// console.log('ids', ids);
+
+// var get = curry(function(prop, obj) {
+//   return obj[prop];
+// });
+
+const get = curry((prop, obj) => obj[prop]);
+console.log('get-name', get('name', obj1));
+console.log('get-id', get('id', obj1));
+
+/**
+ * เพราะ get รับ argument 2 ตัวคือ prop, obj
+ * การ map แต่ละรอบ prop คือ key ที่ต่องการเช่น id, name
+ * และ obj คือ obj แต่ละตัวที่อยู่ใน array [{obj},{obj},{obj}]
+ * ซึ่งจะได้ค่าทุกรอบที่ทำการ map
+ */
+//when use map funtion now invoded like this get('id', obj)
+const getIds = objects.map(get('id'));
+//when use map funtion now invoded like this get('name', obj)
+const getNames = objects.map(get('name'));
+console.log('getIds', getIds);
+console.log('getNames', getNames);
+
+// var map = curry(function(fn, values) {
+//   return values.map(fn);
+// });
+const map = curry((fn, values) => values.map(fn));
